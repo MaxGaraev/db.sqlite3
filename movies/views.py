c6611afaefbb7addbad082c81a290e910b1d8a88
Max Garaev <maxim.garaev@gmail.com>
2022-05-24 02:32:28 +0300

    part 2

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-
-class MoviesView(View):
+class MoviesView(ListView):
     """список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movie_list.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.all()
+    # template_name = 'movies/movie_list.html'
 
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
